f40b3ed9ea233c0ee1b8cd32d197dfc58cc9f5a9
Michael Koch <mitsch.koch@gmail.com>
2022-01-09 13:35:44 +0100

    some more buildin attempts and some new buildin-embedding logic

--- src/Function.hs
+++ src/Function.hs
@@ -9,15 +9,47 @@ import Control.Monad.IO
 import qualified Prelude ( abs )
 
 
-do_error :: [Char] -> [Value] -> [[Type]] -> [Char]
-do_error n vs tts = unlines $  [f, "Expected one of :"] ++ fmap h tts
-    where f = "Got" ++ n ++ "(" ++ (intercalate ", " $ fmap (show . toType) vs) ++ ")"
-          h ts = "\t" ++ n ++ "(" ++ (intercalate ", " $ fmap show ts) ++ ")"
+data Buildin a = Buildin
+    {
+        runBuildin :: a -> [Value] -> [([Char], Value)] -> Either [Char] Value
+    }
 
-builtin_abs :: [Value] -> Either [[Char]] Value
-builtin_abs [IntegerVal x] = return $ IntegerVal $ Prelude.abs x
-builtin_abs [DecimalVal x] = return $ DecimalVal $ Prelude.abs x
-builtin_abs xs = fail $ do_error "abs" [[IntegerType], [DecimalType]] xs
+callBuildin :: a -> Buildin a -> ([Value] -> [([Char], Value)] -> Either [Char] Value)
+callBuildin f b = (runBuildin b) f
+
+overloadBuildin :: ([Value] -> [([Char], Value)] -> Either [Char] Value)
+                -> ([Value] -> [([Char], Value)] -> Either [Char] Value)
+                -> ([Value] -> [([Char], Value)] -> Either [Char] Value)
+overloadBuildin a b = \os ns -> (a os ns) <|> (b os ns)
+
+ret :: (a -> Value) -> Buildin a
+ret f = Buildin $ \x os ns -> pure $ f x
+
+param :: [Char] -> Maybe a -> (Value -> Either [Char] a) -> Buildin b -> Buildin (a -> b)
+param n d f r = Buildin $ \g oss ns -> case oss of
+    { [] -> case lookup n ns of
+        { Nothing -> case d of
+            { Nothing -> Left $ "Parameter \"" ++ n ++ "\" has no positional, named or default argument."
+            ; Just x -> (runBuildin r) (g x) [] ns
+            }
+        ; Just x -> (fmap g (f x)) >>= \y -> (runBuilding r) y [] ns
+        }
+    ; (o:os) -> case lookup n ns of
+        { Nothing -> (fmap g (f o)) >>= \y -> (runBuilding r) y os ns
+        ; Just _ -> Left $ "Parameter \"" ++ n ++ "\" has positional argument as well named argument"
+        }
+    }
+
+
+buildin_abs :: [Value] -> [([Char], Value)] -> Either [Char] Value
+buildin_abs = overloadBuildin _int _float
+    where _int = callBuildin Prelude.abs
+               $ param "x" Nothing expectInteger
+               $ ret IntegerVal
+          _float = callBuildin Prelude.abs
+                 $ param "x" Nothing expectDecimal
+                 $ ret DecimalVal
+
 
 builtin_attr :: [Value] -> Either [[Char]] Value
 builtin_attr [ObjectVal xs, StringVal y] = maybe (fail esmg) return $ lookup y xs
@@ -48,4 +80,57 @@ builtin_default [x, y, BoolVal z] = return $ case (testValue x) || not z of
     ; False -> y
     }
 builtin_default xs = fail $ do_error "default" [[], []] xs
+
+builtin_dictsort :: [Value] -> Either [[Char]] Value
+builtin_dictsort =
+
+builtin_escape :: [Value] -> Either [[Char]] Value
+builtin_escape [StringVal x] = return $ StringVal $ concatMap f x
+    where f '&' = "&amp;"
+          f '<' = "&lt;"
+          f '>' = "&gt;"
+          f '\'' = "&#39;"
+          f '\"' = "&quot;"
+          f x = [x]
+builtin_escape [ObjectVal xs] = maybe (fail e) return $ lookup "__html__" xs
+    where e = ["missing attribute \"__html__\""]
+builtin_escape xs = fail $ do_error "ecape" [[StringType], [ObjectType]] xs
+
+builtin_filesizeformat :: 
+
+builtin_first = 
+
+
+
+_indent :: [Char] -> Int -> Bool -> Bool -> [Char]
+_indent s w True True = unlines $ fmap (f . g) $ lines s
+    where g = dropWhile isSpaces
+          f = ((repeat w ' ')++)
+_indent s w True False = unlines $ fmap () $ lines s
+    where f 
+_indent s w False b = f $ lines s
+    where f (l:ls) = unlines $ (l:) $ fmap (g b) ls
+          f [] = []
+          g True x = ((repeat w ' ')++) $ dropWhile isSpaces x
+          g False x = case all isSpace x of
+                    { True -> x
+                    ; False -> ((repeat w ' ')++) $ dropWhile isSpace x
+                    }
+
+builtin_indent xs = foo ["s", "width", "first", "blank"] $ do
+    { text <- pop >>= expectString
+    ; width <- pop >>= expectInteger <|> (expectString >>= toInteger) <?> 80
+    ; first <- pop >>= expectBoolean <?> False
+    ; blank <- pop >>= expectBoolean <?> False
+    ; return _indent text width first blank
+    }
+
+builtin_indent = _indent
+           `foo` (bar "s" $ expectString)
+           `foo` (bar "width" $ expectInteger <|> (expectString >>= toInteger) <?> 80)
+           `foo` (bar "first" $ expectBoolean <?> False)
+           `foo` (bar "blank" $ expectBoolean < False)
+
+
+
 
