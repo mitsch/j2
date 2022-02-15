8b498ac0f38412ceefd03f53024d06253238a39b
Michael Koch <mitsch.koch@gmail.com>
2022-02-15 16:36:28 +0100

    some small correction

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -1,8 +1,10 @@
 module Evaluation ( Evaluation(..) ) where
 
 import Control.Monad.Fail ( MonadFail, fail )
+import Error ( Error, ErrorTree, throwError, collectError, traceError )
+import Data.List.NonEmpty ( NonEmpty(..) )
 
-data Evaluation a = Evaluation { runEvaluation :: Either [Char] a }
+data Evaluation a = Evaluation { runEvaluation :: Either Error a }
 
 instance Functor (Evaluation) where
     fmap f = Evaluation . fmap f . runEvaluation
@@ -15,6 +17,14 @@ instance Monad (Evaluation) where
     return = Evaluation . Right
     x >>= f = either (Evaluation . Left) f $ runEvaluation x
 
-instance MonadFail (Evaluation) where
-    fail = Evaluation . Left
+instance ErrorTree (Evaluation a) where
+    throwError msg ln pth = Evaluation $ Left $ throwError msg ln pth
+    collectError = Evaluation
+                 . either Right (Left . collectError)
+                 . traverse (either Right Left)
+                 . fmap runEvaluation
+    traceError msg ln pth err = Evaluation
+                              $ either (Left . traceError msg ln pth) Right
+                              $ runEvaluation err
+
 
